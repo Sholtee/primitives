4af12450e5a2fcfc5e0921a7340cdf05a20a8de8
Dénes Solti <sodnaatx@gmail.com>
2020-05-14 10:36:05 +0200

    add pattern: Cache

---
+++ SRC/Cache.cs
@@ -0,0 +1,28 @@
+﻿/********************************************************************************
+* Cache.cs                                                                      *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
+
+namespace Solti.Utils.DI.Internals
+{
+    /// <summary>
+    /// Represents a generic cache where the items have no expiration.
+    /// </summary>
+    public static class Cache 
+    {
+        private static class Backend<TKey, TValue> 
+        {
+            public static ConcurrentDictionary<(TKey Key, string Scope), TValue> Value { get; } = new ConcurrentDictionary<(TKey Key, string Scope), TValue>();     
+        }
+
+        /// <summary>
+        /// Does what its name suggests.
+        /// </summary>
+        public static TValue GetOrAdd<TKey, TValue>(TKey key, Func<TValue> factory, [CallerMemberName] string scope = "") => 
+            Backend<TKey, TValue>.Value.GetOrAdd((key, scope), @void => factory());
+    }
+}
