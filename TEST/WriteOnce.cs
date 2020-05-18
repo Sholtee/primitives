db98a3e411e5dbc9a64beecb9fc8a72806a07bad
Dénes Solti <sodnaatx@gmail.com>
2020-05-18 10:07:36 +0200

    WriteOnce tests

---
+++ TEST/WriteOnce.cs
@@ -0,0 +1,36 @@
+﻿/********************************************************************************
+* WriteOnce.cs                                                                  *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+
+using NUnit.Framework;
+
+namespace Solti.Utils.Primitives.Tests
+{
+    [TestFixture]
+    public class WriteOnceTests
+    {
+        [Test]
+        public void GetValue_ShouldThrowIfTheValueIsNotSet() 
+        {
+            var wo = new WriteOnce();
+
+            object val;
+            Assert.Throws<InvalidOperationException>(() => val = wo.Value);
+
+            wo.Value = new object();
+            Assert.DoesNotThrow(() => val = wo.Value);
+        }
+
+        [Test]
+        public void SetValue_ShouldThrowIfTheValueIsAlreadySet() 
+        {
+            var wo = new WriteOnce();
+
+            Assert.DoesNotThrow(() => wo.Value = new object());
+            Assert.Throws<InvalidOperationException>(() => wo.Value = new object());
+        }
+    }
+}
