f8d5650f3e32d3bc88ff54f617d014193ffb2b9a
Dénes Solti <sodnaatx@gmail.com>
2020-06-17 15:12:04 +0200

    add PropertyInfoExtensions.cs

---
+++ SRC/Public/PropertyInfoExtensions.cs
@@ -0,0 +1,77 @@
+﻿/********************************************************************************
+* PropertyInfoExtensions.cs                                                     *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Solti.Utils.Primitives
+{
+    /// <summary>
+    /// Helper class for creating delegates from <see cref="PropertyInfo"/>.
+    /// </summary>
+    public static class PropertyInfoExtensions // TODO: indexer property support
+    {
+        /// <summary>
+        /// Creates a getter from the given <see cref="PropertyInfo"/>.
+        /// </summary>
+        public static Func<object, object> ToGetter(this PropertyInfo src)
+        {
+            Ensure.Parameter.IsNotNull(src, nameof(src));
+
+            return Cache.GetOrAdd(src, () =>
+            {
+                ParameterExpression p = Expression.Parameter(typeof(object), "instance");
+
+                return Expression
+                    .Lambda<Func<object, object>>
+                    (
+                        Expression.Convert // cast-olas boxibf miatt kell (ha a property visszaterese ValueType)
+                        (
+                            Expression.Property
+                            (
+                                Expression.Convert(p, src.ReflectedType), src
+                            ), 
+                            typeof(object)
+                        ), 
+                        p
+                    )
+                    .Compile();
+            });
+        }
+
+        /// <summary>
+        /// Creates a setter from the given <see cref="PropertyInfo"/>.
+        /// </summary>
+        public static Action<object, object?> ToSetter(this PropertyInfo src)
+        {
+            Ensure.Parameter.IsNotNull(src, nameof(src));
+
+            return Cache.GetOrAdd(src, () =>
+            {
+                ParameterExpression
+                    inst = Expression.Parameter(typeof(object), "instance"),
+                    val  = Expression.Parameter(typeof(object), "value");
+
+                return Expression
+                    .Lambda<Action<object, object?>>
+                    (
+                        Expression.Assign
+                        (
+                            Expression.Property
+                            (
+                                Expression.Convert(inst, src.ReflectedType), 
+                                src
+                            ),
+                            Expression.Convert(val, src.PropertyType)
+                        ),
+                        inst,
+                        val
+                    )
+                    .Compile();
+            });
+        }
+    }
+}
---
+++ TEST/PropertyInfoExtensions.cs
@@ -0,0 +1,33 @@
+﻿/********************************************************************************
+* PropertyInfoExtensions.cs                                                     *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace Solti.Utils.Primitives.Tests
+{
+    [TestFixture]
+    public class PropertyInfoExtensions
+    {
+        [Test]
+        public void Getter_ShouldGetTheDesiredProperty() 
+        {
+            var lst = new List<int>();
+            lst.Add(1986);
+
+            Assert.That(typeof(List<int>).GetProperty(nameof(List<int>.Count)).ToGetter().Invoke(lst), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Setter_ShouldSetTheDesiredProperty()
+        {
+            var lst = new List<int>();
+            typeof(List<int>).GetProperty(nameof(List<int>.Capacity)).ToSetter().Invoke(lst, 1);
+
+            Assert.That(lst.Capacity, Is.EqualTo(1));
+        }
+    }
+}
