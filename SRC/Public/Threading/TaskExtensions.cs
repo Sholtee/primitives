d170d0eeb514591add189d698c948414388b36c9
Dénes Solti <sodnaatx@gmail.com>
2020-07-07 11:34:22 +0200

    add TaskExtensions.cs

---
+++ SRC/Public/Threading/TaskExtensions.cs
@@ -0,0 +1,48 @@
+﻿/********************************************************************************
+* TaskExtensions.cs                                                             *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Solti.Utils.Primitives.Threading
+{
+    /// <summary>
+    /// Exposes several handy methods related to <see cref="Task"/>s.
+    /// </summary>
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// Casts the given <see cref="Task"/>.
+        /// </summary>
+        [SuppressMessage("Reliability", "CA2008:Do not create tasks without passing a TaskScheduler")]
+        public static Task<TT> Cast<T, TT>(this Task<T> task)
+        {
+            Ensure.Parameter.IsNotNull(task, nameof(task));
+
+            return task.ContinueWith(t => 
+            {
+                object result = t.Result!;
+                return Task.FromResult((TT) result);
+            }).Unwrap();
+        }
+
+        /// <summary>
+        /// Casts the given <see cref="Task"/>.
+        /// </summary>
+        public static Task Cast<T>(this Task<T> task, Type returnType) => (Task) Cache
+            .GetOrAdd(returnType, () =>
+            {
+                MethodInfo cast = ((MethodCallExpression) ((Expression<Action>) (() => Cast<object, object>(null!))).Body)
+                    .Method
+                    .GetGenericMethodDefinition();
+
+                return cast.MakeGenericMethod(typeof(T), returnType).ToStaticDelegate();
+            })
+            .Invoke(new object[] { task });
+    }
+}
---
+++ TEST/TaskExtensions.cs
@@ -0,0 +1,44 @@
+﻿/********************************************************************************
+* TaskExtensions.cs                                                             *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace Solti.Utils.Primitives.Tests
+{  
+    using Threading;
+
+    [TestFixture]
+    public class TaskExtensionsTests
+    {
+        [Test]
+        public async Task Cast_ShouldCastTheGivenTaskToThePassedType() 
+        {
+            var task = Task.Factory.StartNew<object>(() => "cica");
+            var stringTask = task.Cast(typeof(string));
+
+            Assert.That(stringTask, Is.InstanceOf<Task<string>>());
+            Assert.That(await (Task<string>) stringTask, Is.EqualTo("cica"));
+        }
+
+        [Test]
+        public async Task Cast_ShouldCastTheGivenTask()
+        {
+            var task = Task.Factory.StartNew<object>(() => "cica");
+            var stringTask = task.Cast<object, string>();
+
+            Assert.That(await stringTask, Is.EqualTo("cica"));
+        }
+
+        [Test]
+        public void Cast_ShouldThrowIfTheCastIsNotPossible()
+        {
+            var task = Task.Factory.StartNew(() => "cica");
+            Assert.ThrowsAsync<InvalidCastException>(() => task.Cast<string, int>());
+        }
+    }
+}
