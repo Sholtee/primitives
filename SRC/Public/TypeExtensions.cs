fa68764fcc895b7dfcdcd88261ed130eb500dd54
Dénes Solti <sodnaatx@gmail.com>
2020-08-18 16:41:24 +0200

    introduce TypeExtensions class

---
+++ SRC/Public/TypeExtensions.cs
@@ -0,0 +1,51 @@
+﻿/********************************************************************************
+* TypeExtensions.cs                                                             *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.IO;
+
+using System.Text.RegularExpressions;
+
+namespace Solti.Utils.Primitives
+{
+    /// <summary>
+    /// Defines extensions for the <see cref="Type"/> type.
+    /// </summary>
+    public static class TypeExtensions
+    {
+        private static readonly Regex Replacer = new Regex("[<>]", RegexOptions.Compiled);
+
+        /// <summary>
+        /// Gets the friendly name of the given type.
+        /// </summary>
+        public static string GetFriendlyName(this Type src)
+        {
+            if (src == null)
+                throw new ArgumentNullException(nameof(src));
+
+            using CodeDomProvider codeDomProvider = CodeDomProvider.CreateProvider("C#");
+            var typeReferenceExpression = new CodeTypeReferenceExpression(new CodeTypeReference(src));
+
+            using var writer = new StringWriter();
+
+            codeDomProvider.GenerateCodeFromExpression(typeReferenceExpression, writer, new CodeGeneratorOptions());
+
+            //
+            // Ezt a nevet meg nem lehet eleresi utvonalakban hasznalni (tartalmaz "<" es ">" karaktereket).
+            //
+
+            string unsafeName = writer.GetStringBuilder().ToString();
+
+            return Replacer.Replace(unsafeName, m => m.Groups[0].Value switch
+            {
+                "<" => "{",
+                ">" => "}",
+                _ => throw new NotImplementedException()
+            });
+        }
+    }
+}
---
+++ TEST/TypeExtensions.cs
@@ -0,0 +1,20 @@
+﻿/********************************************************************************
+* TypeExtensions.cs                                                             *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace Solti.Utils.Primitives.Tests
+{
+    [TestFixture]
+    public class TypeExtensionsTests
+    {
+        [TestCase(typeof(object), "object")]
+        [TestCase(typeof(IList<object>), "System.Collections.Generic.IList{object}")]
+        public void GetFriendlyName_ShouldDoWhatTheNameSuggests(Type type, string name) => Assert.That(type.GetFriendlyName(), Is.EqualTo(name));
+    }
+}
