5802583e971f8c8f37dc0d05d08973dc33c3dc13
Dénes Solti <sodnaatx@gmail.com>
2021-02-17 16:25:44 +0100

    [perf] add some Composite tests

---
+++ PERF/Composite.cs
@@ -0,0 +1,101 @@
+﻿/********************************************************************************
+* Composite.cs                                                                  *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+
+using BenchmarkDotNet.Attributes;
+
+namespace Solti.Utils.Primitives.Perf
+{
+    using Patterns;
+
+    using static Consts;
+
+    [MemoryDiagnoser]
+    public class Composite
+    {
+        private interface IMyComposite : IComposite<IMyComposite> { }
+
+        private class MyComposite : Composite<IMyComposite>, IMyComposite
+        {
+            public MyComposite() : base(null)
+            {
+            }
+        }
+
+        [Benchmark(OperationsPerInvoke = OperationsPerInvoke)]
+        public void Children_Add() 
+        {
+            IMyComposite root = new MyComposite(); // direkt nincs using h a Dispose() hivas ne szamitson bele a meresbe
+            GC.SuppressFinalize(root);
+
+            for (int i = 0; i < OperationsPerInvoke; i++)
+            {
+                IMyComposite child = new MyComposite();
+                GC.SuppressFinalize(child);
+
+                root.Children.Add(child);
+            }
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class Composite_Dispatch
+    {
+        private interface IMyComposite : IComposite<IMyComposite> 
+        {
+            void Foo(string arg);
+        }
+
+        private class MyComposite : Composite<IMyComposite>, IMyComposite
+        {
+            public MyComposite() : base(null)
+            {
+            }
+
+            void IMyComposite.Foo(string arg) => Dispatch(null, arg);
+        }
+
+        [Params(1, 2, 5, 10)]
+        public int Depth { get; set; }
+
+        [Params(1, 2, 5, 10, 100)]
+        public int ChildCount { get; set; }
+
+        private IMyComposite Root { get; set; }
+
+        [GlobalSetup(Target = nameof(Dispatch))]
+        public void Setup() 
+        {
+            Root = new MyComposite();
+            GC.SuppressFinalize(Root);
+
+            AddChildren(Root, 0);
+
+            void AddChildren(IMyComposite current, int currentDepth)
+            {
+                for (int i = 0; i < ChildCount; i++)
+                {
+                    IMyComposite child = new MyComposite();
+                    GC.SuppressFinalize(child);
+
+                    if (currentDepth < Depth)
+                        AddChildren(child, currentDepth + 1);
+
+                    current.Children.Add(child);
+                }
+            }
+        }
+
+        [Benchmark(OperationsPerInvoke = OperationsPerInvoke)]
+        public void Dispatch()
+        {
+            for (int i = 0; i < OperationsPerInvoke; i++)
+            {
+                Root.Foo("cica");
+            }
+        }
+    }
+}
--- PERF/index.md
+++ PERF/index.md
@@ -1,3 +1,5 @@
 # Solti.Utils.Primitives Benchmark Results
 
 - [StackFrame](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.StackFrame-report-github.html )
+- [Composite](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.Composite-report-github.html )
+- [Composite_Dispatch](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.Composite_Dispatch-report-github.html )
