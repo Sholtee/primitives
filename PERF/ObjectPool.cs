c6298337a5568d01e1ab27746aacff35bdb86783
Dénes Solti <sodnaatx@gmail.com>
2021-02-18 19:21:44 +0100

    [perf] add an ObjectPool test

---
+++ PERF/ObjectPool.cs
@@ -0,0 +1,27 @@
+﻿/********************************************************************************
+* ObjectPool.cs                                                                 *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using BenchmarkDotNet.Attributes;
+
+namespace Solti.Utils.Primitives.Perf
+{
+    using static Consts;
+
+    [MemoryDiagnoser]
+    public class ObjectPool
+    {
+        [Benchmark(OperationsPerInvoke = OperationsPerInvoke)]
+        public void GetAndReturn() 
+        {
+            using var pool = new Patterns.ObjectPool<object>(1, () => new object());
+
+            for (int i = 0; i < OperationsPerInvoke; i++)
+            {
+                pool.Get(Patterns.CheckoutPolicy.Throw);
+                pool.Return();
+            }
+        }
+    }
+}
--- PERF/index.md
+++ PERF/index.md
@@ -3,3 +3,4 @@
 - [StackFrame](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.StackFrame-report-github.html )
 - [Composite](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.Composite-report-github.html )
 - [Composite_Dispatch](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.Composite_Dispatch-report-github.html )
+- [ObjectPool](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.ObjectPool-report-github.html )
