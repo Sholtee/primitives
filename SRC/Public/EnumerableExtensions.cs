381f8efc5ecda9fa5c5bd7046da9b30e7f3b32d2
Dénes Solti <sodnaatx@gmail.com>
2021-02-18 11:07:19 +0100

    introduce EnumerableExtensions class

---
+++ SRC/Public/EnumerableExtensions.cs
@@ -0,0 +1,28 @@
+﻿/********************************************************************************
+* EnumerableExtensions.cs                                                       *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Collections.Generic;
+
+namespace Solti.Utils.Primitives
+{
+    /// <summary>
+    /// Defines some extensions for the <see cref="IEnumerable{T}"/> interface.
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Iterates through the source collection.
+        /// </summary>
+        public static void ForEach<T>(this IEnumerable<T> src, Action<T, int> callback)
+        {
+            int i = 0;
+            foreach (T item in src ?? throw new ArgumentNullException(nameof(src)))
+            {
+                callback(item, i++);
+            }
+        }
+    }
+}
