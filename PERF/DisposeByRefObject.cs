9d56d035b7c203b412b456b8689aa6687e76a734
Dénes Solti <sodnaatx@gmail.com>
2021-02-19 05:41:23 +0100

    [perf] add an DisposeByRefObject test

---
+++ PERF/DisposeByRefObject.cs
@@ -0,0 +1,26 @@
+﻿/********************************************************************************
+* DisposeByRefObject.cs                                                         *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using BenchmarkDotNet.Attributes;
+
+namespace Solti.Utils.Primitives.Perf
+{
+    using static Consts;
+
+    [MemoryDiagnoser]
+    public class DisposeByRefObject
+    {
+        [Benchmark(OperationsPerInvoke = OperationsPerInvoke)]
+        public void AddRef() 
+        {
+            var obj = new Patterns.DisposeByRefObject();
+
+            for (int i = 0; i < OperationsPerInvoke; i++)
+            {
+                obj.AddRef();
+            }
+        }
+    }
+}
--- PERF/index.md
+++ PERF/index.md
@@ -1,6 +1,7 @@
 # Solti.Utils.Primitives Benchmark Results
 
-- [StackFrame](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.StackFrame-report-github.html )
 - [Composite](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.Composite-report-github.html )
 - [Composite_Dispatch](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.Composite_Dispatch-report-github.html )
+- [DisposeByRefObject](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.DisposeByRefObject-report-github.html )
 - [ObjectPool](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.ObjectPool-report-github.html )
+- [StackFrame](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.StackFrame-report-github.html )
