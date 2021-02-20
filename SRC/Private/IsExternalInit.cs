a11cf033dffe69f2ac7615b0c88e14d90b3bebc5
Dénes Solti <sodnaatx@gmail.com>
2021-02-20 19:02:30 +0100

    #1 [declare IsExternalInit for all TFMs]

--- SRC/Private/IsExternalInit.cs
+++ SRC/Private/IsExternalInit.cs
@@ -3,12 +3,15 @@
 *                                                                               *
 * Author: Denes Solti                                                           *
 ********************************************************************************/
-#if NETSTANDARD
+
+//
+// NET5_0 elotti runtime-on a C# 9 init accessor hasznalata csak hack reven megoldott:
+// https://stackoverflow.com/questions/62648189/testing-c-sharp-9-0-in-vs2019-cs0518-isexternalinit-is-not-defined-or-imported
+// Ebben az esetben viszont az osszes TFM szamara definialni kell az IsExternalInit osztalyt:
+// https://twitter.com/aarnott/status/1362786409954766858
+//
+
 namespace System.Runtime.CompilerServices
 {
-    /// <summary>
-    /// https://stackoverflow.com/questions/62648189/testing-c-sharp-9-0-in-vs2019-cs0518-isexternalinit-is-not-defined-or-imported
-    /// </summary>
     internal class IsExternalInit { }
 }
-#endif
