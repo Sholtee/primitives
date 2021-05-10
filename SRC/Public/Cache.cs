0b236df1bb0a87ba4aa8c6c5de2a334cc476ff92
Dénes Solti <sodnaatx@gmail.com>
2021-05-10 19:43:24 +0200

    Cache.GetOrAdd() fix [prevent the factory from being called multiple times]

--- SRC/Public/Cache.cs
+++ SRC/Public/Cache.cs
@@ -7,7 +7,9 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace Solti.Utils.Primitives
 {
@@ -19,18 +21,27 @@ namespace Solti.Utils.Primitives
     {
         private static class Backend<TKey, TValue> 
         {
-            public static ConcurrentDictionary<(TKey, string), TValue> Value { get; } = new ConcurrentDictionary<(TKey, string), TValue>();     
+            public static ConcurrentDictionary<(TKey, string), Lazy<TValue>> Value { get; } = new();     
         }
 
         /// <summary>
         /// Gets the underlying dictionary associated with the given key and value type.
         /// </summary>
-        public static IDictionary<(TKey, string), TValue> AsDictionary<TKey, TValue>() => Backend<TKey, TValue>.Value;
+        /// <remarks>This method returns a snapshot.</remarks>
+        public static IDictionary<(TKey, string), TValue> AsDictionary<TKey, TValue>() => Backend<TKey, TValue>
+            .Value
+            .ToDictionary(entry => entry.Key, entry => entry.Value.Value);
 
         /// <summary>
         /// Does what its name suggests.
         /// </summary>
-        public static TValue GetOrAdd<TKey, TValue>(TKey key, Func<TValue> factory, [CallerMemberName] string scope = "") => 
-            Backend<TKey, TValue>.Value.GetOrAdd((key, scope), @void => factory());
+        public static TValue GetOrAdd<TKey, TValue>(TKey key, Func<TValue> factory, [CallerMemberName] string scope = "") =>  Backend<TKey, TValue>
+            .Value
+            //
+            // Ne a GetOrAdd(Key, Factory) overload-jat hasznaljuk mert ott a factory tobbszor is meghivasra kerulhet
+            // parhuzamos esetben: https://docs.microsoft.com/en-us/dotnet/api/system.collections.concurrent.concurrentdictionary-2.getoradd
+            //
+
+            .GetOrAdd((key, scope), new Lazy<TValue>(factory, LazyThreadSafetyMode.ExecutionAndPublication)).Value;
     }
 }
