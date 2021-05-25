45619708141f5f1b4a5412feceef31f26da819d6
Dénes Solti <sodnaatx@gmail.com>
2021-05-25 13:17:47 +0200

    introduce ExpressionExtensions class

---
+++ SRC/Public/ExpressionExtensions.cs
@@ -0,0 +1,26 @@
+﻿/********************************************************************************
+* ExpressionExtensions.cs                                                       *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Solti.Utils.Primitives
+{
+    /// <summary>
+    /// Contains some <see cref="Expression"/> related helper methods.
+    /// </summary>
+    public static class ExpressionExtensions
+    {
+        private static readonly Func<object, object?> GetDebugViewCore = typeof(Expression)
+            .GetProperty("DebugView", BindingFlags.Instance | BindingFlags.NonPublic) // DebugView property internal, tudja fasz miert
+            .ToGetter();
+
+        /// <summary>
+        /// Gets the debug view of the given <paramref name="expression"/>.
+        /// </summary>
+        public static string GetDebugView(this Expression expression) => (string) GetDebugViewCore(expression ?? throw new ArgumentNullException(nameof(expression)))!;
+    }
+}
