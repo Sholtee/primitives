6267cf7fe9b890a183a2b0c530d0a530ea52818b
Dénes Solti <sodnaatx@gmail.com>
2021-08-12 13:53:53 +0200

    introduce INotifyOnDispose interface & DisposableSupportsNotifyOnDispose class

---
+++ SRC/Public/Patterns/DisposableSupportsNotifyOnDispose.cs
@@ -0,0 +1,25 @@
+﻿/********************************************************************************
+* DisposableSupportsNotifyOnDispose.cs                                          *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+
+namespace Solti.Utils.Primitives.Patterns
+{
+    /// <summary>
+    /// Describes a disposable object that implements the <see cref="INotifyOnDispose"/> interface
+    /// </summary>
+    public class DisposableSupportsNotifyOnDispose : Disposable, INotifyOnDispose
+    {
+        /// <inheritdoc/>
+        public event EventHandler? OnDispose;
+
+        /// <inheritdoc/>
+        protected override void BeforeDispose()
+        {
+            base.BeforeDispose();
+            OnDispose?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ SRC/Public/Patterns/INotifyOnDispose.cs
@@ -0,0 +1,20 @@
+﻿/********************************************************************************
+* INotifyOnDispose.cs                                                           *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+
+namespace Solti.Utils.Primitives.Patterns
+{
+    /// <summary>
+    /// Notifies the clients if the object is about to dispose.
+    /// </summary>
+    public interface INotifyOnDispose
+    {
+        /// <summary>
+        /// Fires if the object is about to dispose.
+        /// </summary>
+        event EventHandler? OnDispose;
+    }
+}
