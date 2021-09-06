060a5f967469e24a0e19c003db2ebe184067930e
Dénes Solti <sodnaatx@gmail.com>
2021-09-06 15:42:11 +0200

    add perf test for ExclusiveBlock

---
+++ PERF/ExclusiveBlock.cs
@@ -0,0 +1,35 @@
+﻿/********************************************************************************
+* ExclusiveBlock.cs                                                             *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using BenchmarkDotNet.Attributes;
+
+namespace Solti.Utils.Primitives.Perf
+{
+    using Threading;
+
+    [MemoryDiagnoser]
+    public class ExclusiveBlock
+    {
+        public Threading.ExclusiveBlock Block { get; set; }
+
+        [Params(ExclusiveBlockFeatures.None, ExclusiveBlockFeatures.SupportsRecursion)]
+        public ExclusiveBlockFeatures Features { get; set; }
+
+        [GlobalSetup]
+        public void Setup() => Block = new(Features);
+
+        [GlobalCleanup]
+        public void Cleanup() => Block?.Dispose();
+
+        [Benchmark]
+        public void EnterAndLeave()
+        {
+            using (Block.Enter())
+            {
+                _ = Block.Features;
+            }
+        }
+    }
+}
--- PERF/index.md
+++ PERF/index.md
@@ -2,6 +2,7 @@
 
 - [Composite](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.Composite-report-github.html )
 - [Composite_Dispatch](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.Composite_Dispatch-report-github.html )
-- [DisposeByRefObject](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.DisposeByRefObject-report-github.html )
 - [ConcurrentLinkedList](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.LinkedList-report-github.html )
+- [DisposeByRefObject](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.DisposeByRefObject-report-github.html )
+- [ExclusiveBlock](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.ExclusiveBlock-report-github.html )
 - [ObjectPool](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.ObjectPool_Comparison-report-github.html )
