a0dd632d7ff79620b38eb96752de7a590273d99f
Dénes Solti <sodnaatx@gmail.com>
2022-04-28 12:27:36 +0200

    fox Cache key collision issue

--- SRC/Public/Cache.cs
+++ SRC/Public/Cache.cs
@@ -5,6 +5,7 @@
 ********************************************************************************/
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 
@@ -18,11 +19,7 @@ namespace Solti.Utils.Primitives
     {
         private static class Backend<TKey, TValue> 
         {
-            //
-            // Dictionary performs much better against int keys
-            //
-
-            private static readonly ConcurrentDictionary<int, LazySlim> FImplementation = new();
+            private static readonly ConcurrentDictionary<CacheEntry, CacheEntry> FImplementation = new();
 
             //
             // We don't use factory function here since it may get called more than once if the GetOrAdd()
@@ -30,16 +27,22 @@ namespace Solti.Utils.Primitives
             // https://docs.microsoft.com/en-us/dotnet/api/system.collections.concurrent.concurrentdictionary-2.getoradd
             //
 
-            private sealed class LazySlim
+            private sealed class CacheEntry
             {
                 private readonly Func<TKey, TValue> FFactory;
-                private readonly TKey FContext;
+                private readonly TKey FKey;
+                private readonly string FScope;
+                private readonly int FHashCode;
                 private TValue? FValue;
 
-                public LazySlim(Func<TKey, TValue> factory, TKey context)
+                public CacheEntry(TKey key, string scope, Func<TKey, TValue> factory)
                 {
                     FFactory = factory;
-                    FContext = context;
+                    FKey = key;
+                    FScope = scope;
+                    #pragma warning disable CA1307 // Specify StringComparison for clarity
+                    FHashCode = unchecked((key?.GetHashCode() ?? 0) ^ (scope?.GetHashCode() ?? 0));
+                    #pragma warning restore CA1307
                 }
 
                 public TValue Value
@@ -48,19 +51,35 @@ namespace Solti.Utils.Primitives
                     {
                         if (FValue is null)
                             lock (FFactory)
-                                FValue ??= FFactory(FContext);
+                                FValue ??= FFactory(FKey);
                         return FValue;
                     }
                 }
+
+                public override int GetHashCode() => FHashCode;
+
+                public override bool Equals(object obj)
+                {
+                    CacheEntry that = (CacheEntry) obj;
+
+                    return
+                        that.FHashCode == FHashCode &&
+
+                        //
+                        // Comparing the hashcode is not enough since special types (e.g.: delegates) cannot be distinguished by hash.
+                        //
+
+                        EqualityComparer<TKey>.Default.Equals(that.FKey, FKey) && 
+                        that.FScope == FScope;
+                }
             }
 
-            public static TValue GetOrAdd(TKey key, Func<TKey, TValue> factory, string scope) => FImplementation.GetOrAdd
-            (
-                #pragma warning disable CA1307 // Specify StringComparison for clarity
-                unchecked((key?.GetHashCode() ?? 0) ^ (scope?.GetHashCode() ?? 0)),
-                #pragma warning restore CA1307
-                new LazySlim(factory, key)
-            ).Value;
+            public static TValue GetOrAdd(TKey key, string scope, Func<TKey, TValue> factory)
+            {
+                CacheEntry entry = new(key, scope, factory);
+
+                return FImplementation.GetOrAdd(entry,  entry).Value;
+            }
 
             public static void Clear() => FImplementation.Clear();
         }
@@ -73,6 +92,6 @@ namespace Solti.Utils.Primitives
         /// <summary>
         /// Does what its name suggests.
         /// </summary>
-        public static TValue GetOrAdd<TKey, TValue>(TKey key, Func<TKey, TValue> factory, [CallerMemberName] string scope = "") => Backend<TKey, TValue>.GetOrAdd(key, factory, scope);
+        public static TValue GetOrAdd<TKey, TValue>(TKey key, Func<TKey, TValue> factory, [CallerMemberName] string scope = "") => Backend<TKey, TValue>.GetOrAdd(key, scope, factory);
     }
 }
