e0fe54f754c058e9f4ba2595aa9779a7a282be73
Dénes Solti <sodnaatx@gmail.com>
2022-08-30 09:10:30 +0200

    test RedBlackTree performance

---
+++ PERF/RedBlackTree.cs
@@ -0,0 +1,66 @@
+﻿/********************************************************************************
+* RedBlackTree.cs                                                               *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System.Collections.Generic;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+
+namespace Solti.Utils.Primitives.Perf
+{
+    using Patterns;
+
+    [MemoryDiagnoser]
+    [SimpleJob(RunStrategy.Throughput, targetCount: 15, invocationCount: 300)]
+    public class RedBlackTree
+    {
+        private sealed class IntComparer : Singleton<IntComparer>, IComparer<int>
+        {
+            public int Compare(int x, int y) => x.CompareTo(y);
+        }
+
+        public RedBlackTree<int> Tree { get; set; }
+
+        [GlobalSetup(Target = nameof(Add))]
+        public void SetupAdd() => Tree = new RedBlackTree<int>(IntComparer.Instance);
+
+        [Benchmark(OperationsPerInvoke = 10000)]
+        public void Add()
+        {
+            for (int i = 0; i < 10000; i++)
+                Tree.Add(i);
+        }
+
+        [GlobalSetup(Target = nameof(With))]
+        public void SetupWith() => Tree = new RedBlackTree<int>(IntComparer.Instance);
+
+        [Benchmark(OperationsPerInvoke = 500)]
+        public void With()
+        {
+            RedBlackTree<int> tree = Tree;
+            for (int i = 0; i < 500; i++)
+                tree = tree.With(i);
+        }
+
+        [GlobalSetup(Target = nameof(Enumerate_MoveNext))]
+        public void SetupEnumerate()
+        {
+            Tree = new RedBlackTree<int>(IntComparer.Instance);
+
+            for (int i = 0; i < 100000; i++)
+                Tree.Add(i);
+        }
+
+        [Benchmark(OperationsPerInvoke = 100000)]
+        public void Enumerate_MoveNext()
+        {
+            using IEnumerator<RedBlackTreeNode<int>> enumerator = Tree.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                _ = enumerator.Current;
+            }
+        }
+    }
+}
--- PERF/index.md
+++ PERF/index.md
@@ -6,3 +6,4 @@
 - [DisposeByRefObject](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.DisposeByRefObject-report-github.html )
 - [ExclusiveBlock](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.ExclusiveBlock-report-github.html )
 - [ObjectPool](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.ObjectPool_Comparison-report-github.html )
+- [RedBlackTree](https://sholtee.github.io/primitives/perf/Solti.Utils.Primitives.Perf.RedBlackTree-report.html )
